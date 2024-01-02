3e571198d2ae82531bc6aa4e1c2b5e12c324687e
fardeenhasib <fardeenmozumder@gmail.com>
2024-01-02 16:20:43 -0600

    Quiz game completed

--- src/App.js
+++ src/App.js
@@ -2,102 +2,64 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import "./App.css";
 
-const WORD_LIST_API_URL = 'https://api.frontendexpert.io/api/fe/wordle-words';
-const TOTAL_CHANCES = 6;
-const WORD_LENGTH = 5;
 
+const QUIZ_API_BASE_URL = 'https://api.frontendexpert.io/api/fe/quiz';
 
-export default function Wordle() {
+export default function Quiz() {
   // Write your code here.
-  const [guesses, setGuesses] = useState(Array(TOTAL_CHANCES).fill(null));
-  const [currentGuess, setCurrentGuess] = useState('');
-  const [solution, setSolution] = useState(null);
+  const [questions, setQuestions] = useState(null);
+  const [curIndex, setCurIndex] = useState(0);
+  const [ansArr, setAnsArr] = useState([]);
 
   useEffect(() => {
-    const fetchResult = async () => {
-      const response = await fetch(WORD_LIST_API_URL);
-      const words = await response.json();
-      setSolution(words[Math.floor(Math.random() * words.length)].toLowerCase());
+    const getQs = async () => {
+      const fetchedQs = await fetch(QUIZ_API_BASE_URL);
+      const Qs = await fetchedQs.json();
+      setQuestions(Qs);
     }
-    fetchResult();
+    getQs();
   }, []);
 
-  useEffect(() => {
-    // to handle keypress
+  const updateAnswer = (qIndex, ansIndex) => {
+    const ansArrClone = [...ansArr];
+    ansArrClone[qIndex] = ansIndex;
+    setAnsArr(ansArrClone);
+  }
 
-    if (solution == null) return;
-
-    const handlePress = event => {
-      if (guesses[TOTAL_CHANCES - 1] != null || guesses.includes(solution)) {
-        return;
-      }
-
-      const newKeyCharCode = event.key.toLowerCase().charCodeAt(0);
-      const isLetter =
-        event.key.length === 1 &&
-        newKeyCharCode >= 'a'.charCodeAt(0) &&
-        newKeyCharCode <= 'z'.charCodeAt(0);
-
-      setCurrentGuess((prevGuess) => {
-        if (event.key === "Backspace") {
-          return prevGuess.slice(0, -1);
-        } else if (event.key === "Enter" && prevGuess.length === WORD_LENGTH) {
-          // guesslist update korte hobe
-          const currentGuessIndex = guesses.findIndex(guess => guess == null);
-          const guessClone = [...guesses];
-          guessClone[currentGuessIndex] = prevGuess;
-          setGuesses(guessClone);
-          return '';
-        } else if (prevGuess.length < WORD_LENGTH && isLetter) {
-          return prevGuess + event.key.toLowerCase();
+  if (questions == null) return null;
+  const curQuestion = questions[curIndex];
+  return (
+    <>
+      <h1>{curQuestion.question}</h1>
+      {curQuestion.answers.map((answer, index) => {
+        let className = 'answer';
+        let chosenAnswer = ansArr[curIndex];
+        if (chosenAnswer === index) {
+          className += curQuestion.correctAnswer === chosenAnswer ? ' correct' : ' incorrect';
         }
-        return prevGuess;
+        console.log(className);
+        return (
+          <h2
+            key={answer}
+            className={className}
+            onClick={() => {
+              if (ansArr[curIndex] != null) {
+                return;
+              }
+              updateAnswer(curIndex, index);
+            }}>{answer}</h2>
 
-      });
-    };
-
-    window.addEventListener("keydown", handlePress);
-    return () => window.removeEventListener("keydown", handlePress);
-  }, [solution, guesses]);
-
-
-  const currentGuessIndex = guesses.findIndex(guess => guess == null);
-
-  if (solution == null) return null;
-
-  return (
-    <div className="board">
-      {/* Write your code here. */
-        guesses.map((guess, i) => {
-          return (
-            <GuessLine
-              key={i}
-              guess={(i === currentGuessIndex ? currentGuess : guess ?? '').padEnd(WORD_LENGTH)}
-              isFinal={i < currentGuessIndex || currentGuessIndex === -1}
-              solution={solution}
-            />
-          );
-        })
-      }
-    </div>
-  );
-}
-
-function GuessLine({ guess, solution, isFinal }) {
-  return (
-    <div className="line">
-      {
-        guess.split('').map((char, i) => {
-          let className = 'tile';
-          if (isFinal) {
-            if (solution[i] === char) { className += " correct"; }
-            else if (solution.includes(char)) { className += " close"; }
-            else { className += " incorrect"; }
-          }
-
-          return <div key={i} className={className}>{char}</div>
-        })
-      }
-    </div>
+        );
+      })}
+      <button onClick={() => {
+        setCurIndex(curIndex - 1);
+      }}
+        disabled={curIndex == 0}>Back</button>
+      <button
+        onClick={() => {
+          setCurIndex(curIndex + 1);
+        }}
+        disabled={curIndex == questions.length - 1 || ansArr[curIndex] == null}>Next</button>
+    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import QuestionList from './App';
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <QuestionList />
+    <App />
   </React.StrictMode>
 );
 
